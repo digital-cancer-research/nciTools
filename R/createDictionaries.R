c1ecb99b3892bc774ebce676a5a438e3e3bb2719
Paul O'Regan <paul.oregan@digitalecmt.org>
2025-10-01 16:46:06 +0100

    amend DESCRIPTION, add header to functions

--- R/createDictionaries.R
+++ R/createDictionaries.R
@@ -1,3 +1,11 @@
+# Copyright 2025 Cancer Research Technology Limited.
+#
+# Licensed under a software academic use license provided with this software package (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at [link to package location of the license]
+# For commercial use, please contact Cancer Research Horizons at crh-cpmarketing@cancer.org.uk
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
 #' Create dictionaries based on NCI thesaurus
 #'
 #' @description
